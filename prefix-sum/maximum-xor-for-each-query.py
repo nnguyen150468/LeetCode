7c40651a1c8c2baa4a1b393b8a6c897457ff4f98
Nguyen Nguyen <59470567+nnguyen150468@users.noreply.github.com>
2024-11-12 23:56:15 -0500

    LC-maximum-xor-for-each-query [Runtime: 94 ms; Memory: 32.3 MB]

--- prefix-sum/maximum-xor-for-each-query.py
+++ prefix-sum/maximum-xor-for-each-query.py
@@ -10,10 +10,9 @@ class Solution:
          |0
         |0|
         '''
-        xor_table = [nums[0]] * len(nums)
         for i in range(1, len(nums)):
-            xor_table[i] = xor_table[i-1] ^ nums[i]
+            nums[i] = nums[i-1] ^ nums[i]
         res = []
         for i in range(len(nums)-1,-1,-1):
-            res.append(xor_table[i] ^ (2**maximumBit - 1))
+            res.append(nums[i] ^ (2**maximumBit - 1))
         return res
