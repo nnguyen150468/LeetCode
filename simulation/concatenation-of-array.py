a7f69cb71aa9c0bb457f97e91408553cfc45d9eb
Nguyen Nguyen <59470567+nnguyen150468@users.noreply.github.com>
2026-01-16 09:29:57 -0500

    LC-concatenation-of-array [Runtime: 3 ms; Memory: 19.6 MB]

--- simulation/concatenation-of-array.py
+++ simulation/concatenation-of-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = [0] * (len(nums) * 2)
-        for i in range(len(ans)):
-            ans[i] = nums[i % len(nums)]
+        ans = []
+        for i in range(len(nums) * 2):
+            ans.append(nums[i % len(nums)])
         return ans
