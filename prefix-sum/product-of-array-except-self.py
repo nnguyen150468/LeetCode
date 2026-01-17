d13781bc01c9496b3bacb7a6b86ccfe39cf05edd
Nguyen Nguyen <59470567+nnguyen150468@users.noreply.github.com>
2026-01-17 15:42:11 -0500

    LC-product-of-array-except-self [Runtime: 23 ms; Memory: 25.1 MB]

--- prefix-sum/product-of-array-except-self.py
+++ prefix-sum/product-of-array-except-self.py
@@ -3,19 +3,22 @@ class Solution:
         '''
         [1  2  3  4]
         [1  1  1  1]
-        [24 12 4 1]
-        [1, 1, 2, 6]
+        
+        [1  1  2  6]
+            12   8   6  
+
+        suffix = 1
+                24
 
         
         [      8   6 ]
         '''
         prefix = [1] * len(nums)
-        suffix = [1] * len(nums)
+        suffix = 1
         for i in range(1, len(nums)):
             prefix[i] = nums[i-1] * prefix[i-1]
         for i in range(len(nums)-2, -1, -1):
-            suffix[i] = nums[i+1] * suffix[i+1]
-        res = []
-        for i in range(len(nums)):
-            res.append(prefix[i] * suffix[i])
-        return res
+            suffix *= nums[i+1]
+            prefix[i] *= suffix
+
+        return prefix
